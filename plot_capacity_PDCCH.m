991705396b8f96dba09eec2da1b871e51de20080
Rob Maunder <rob.maunder@accelercomm.com>
2018-01-17 12:54:49 +0000

    Fixed a typo.

--- plot_capacity_PDCCH.m
+++ plot_capacity_PDCCH.m
@@ -1,13 +1,13 @@
 addpath '../spectre/bi-awgn'
 
-    A = [8:15, 16:2:30, 32:4:60, 64:8:120, 128:16:140];
-    E = [108 216 432 864 1728];
+A = [8:15, 16:2:30, 32:4:60, 64:8:120, 128:16:140];
+E = [108 216 432 864 1728];
 
-    
-    K = A+21;
-    
-    
-    
+
+K = A+21;
+
+
+
 % Create a figure to plot the results.
 figure
 axes1 = axes;
@@ -19,10 +19,10 @@ set (gca, 'XTickLabel', 2.^xt);
 grid on
 hold on
 drawnow
-    
+
 for E_index = 1:length(E)
     
-        % Create the plot
+    % Create the plot
     plots(E_index) = plot(nan,'Parent',axes1);
     legend(cellstr(num2str(E(1:E_index)', 'E=%d, capacity')),'Location','eastoutside');
     
@@ -30,8 +30,8 @@ for E_index = 1:length(E)
     
     plot_EsN0s = [];
     plot_As = [];
-        for A_index = 1:length(A)
-            if K(A_index)+3 <= E(E_index)
+    for A_index = 1:length(A)
+        if K(A_index)+3 <= E(E_index)
             EsN0 = converse_mc(E(E_index), 0.001, K(A_index)/E(E_index),'On2','error');
             if isempty(plot_EsN0s) || EsN0 > plot_EsN0s(end)
                 plot_EsN0s(end+1) = EsN0;
@@ -40,16 +40,15 @@ for E_index = 1:length(E)
                 % Plot the SNR vs A results
                 set(plots(E_index),'XData',log2(plot_As));
                 set(plots(E_index),'YData',plot_EsN0s);
-
+                
                 xlim auto;
                 xl = xlim;
                 xlim([floor(xl(1)), ceil(xl(2))]);
-
+                
                 drawnow;
                 
             end
-                        
-            end
-            end
+            
         end
+    end
 end
